9cabee963ee68778f85c7770a3be2f51b81c148b
tuanboidoi29-ai <tuanboidoi29@gmail.com>
2026-08-25 21:05:32 +0700

    Add TT 1.0.5 release loader

---
+++ release_1.0.5/tt_tao_van.rb
@@ -0,0 +1,23 @@
+# ============================================================
+# TT - TẠO VÁN - TRẦN TUẤN
+# Root loader
+# ============================================================
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module TranTuan
+  module TaoVan
+    EXTENSION = SketchupExtension.new(
+      'TT - tạo ván -Trần Tuấn',
+      'TT_TaoVan/main'
+    )
+    EXTENSION.version = '1.0.5'
+    EXTENSION.creator = 'TRẦN TUẤN'
+    EXTENSION.description = 'Tạo ván Face 3D, Tạo Box, Tạo Ngăn Kéo, Xuất chi tiết ván và Hot Update.'
+    EXTENSION.copyright = '© 2026 TRẦN TUẤN'
+    unless defined?(@registered) && @registered
+      Sketchup.register_extension(EXTENSION, true)
+      @registered = true
+    end
+  end
+end
