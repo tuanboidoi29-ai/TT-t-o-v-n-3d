9535d120e4b72594c082b0b92b17616bcf298a6b
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 02:15:15 +0700

    Add Layout statistics toggle to settings

--- tran_tuan_noi_that_release/files/tran_tuan_noi_that/settings.rb
+++ tran_tuan_noi_that_release/files/tran_tuan_noi_that/settings.rb
@@ -17,7 +17,7 @@ module TranTuanNoiThat
         TranTuanNoiThat.save_setting('thickness', thickness)
         TranTuanNoiThat.save_setting('auto_update', !!data['auto_update'])
         TranTuanNoiThat.save_setting('update_channel', data['channel'].to_s)
-        %w[board box drawer round grain].each do |feature|
+        %w[board box drawer round grain layout_stats].each do |feature|
           TranTuanNoiThat.save_setting("feature_#{feature}", !!data["feature_#{feature}"])
         end
         TranTuanNoiThat.refresh_feature_commands
@@ -43,7 +43,8 @@ module TranTuanNoiThat
         feature_box: TranTuanNoiThat.feature_enabled?(:box),
         feature_drawer: TranTuanNoiThat.feature_enabled?(:drawer),
         feature_round: TranTuanNoiThat.feature_enabled?(:round),
-        feature_grain: TranTuanNoiThat.feature_enabled?(:grain)
+        feature_grain: TranTuanNoiThat.feature_enabled?(:grain),
+        feature_layout_stats: TranTuanNoiThat.feature_enabled?(:layout_stats)
       }
       @dialog.execute_script("window.setSettings(#{JSON.generate(payload)})")
     end
