11d30997fe5373d71b8994d7205d892327c914cc
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-15 17:35:09 +0700

    Sync source loader to 1.9.4

--- tran_tuan_noi_that_release/src/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/src/TranTuanNoiThat.rb
@@ -6,7 +6,7 @@ module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
     ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Vẽ Ván, BOX, Ngăn Kéo, Bo Cong Khối và cập nhật nóng từ GitHub.'
-    ext.version = '1.9.0'
+    ext.version = '1.9.4'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
