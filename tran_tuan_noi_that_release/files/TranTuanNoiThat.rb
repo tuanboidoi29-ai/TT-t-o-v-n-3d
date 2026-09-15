a2149536a9bc5cba42d0aedc79490fe9253be681
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 03:20:01 +0700

    Bump system loader to 1.9.32 for online update

--- tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
@@ -6,7 +6,7 @@ module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
     ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Vẽ Ván, BOX, Ngăn Kéo, Bo Cong Khối, Co Giãn Khối MODE, Xoay Vân Ván, Xuất Layout + Thống Kê Ván với xem trước toàn bộ Layout A3 ngay trong bảng, mặt cắt và Line/X-Ray.'
-    ext.version = '1.9.31'
+    ext.version = '1.9.32'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
