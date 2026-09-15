0e15013020890aaaf6da5a80f1b1e9ba21346035
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 04:13:42 +0700

    Bump system to 1.9.38 LayoutStats V0.7.0

--- tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
@@ -5,8 +5,8 @@ require 'extensions.rb'
 module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
-    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Vẽ Ván, BOX, Ngăn Kéo, Bo Cong Khối, Co Giãn Khối MODE, Xoay Vân chuẩn 2440x1220 không khóa màu Face, Xuất Layout + Thống Kê Ván V0.6.'
-    ext.version = '1.9.37'
+    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Vẽ Ván, BOX, Ngăn Kéo, Bo Cong Khối, Co Giãn Khối MODE, Xoay Vân chuẩn 2440x1220, Xuất Layout chỉ phối cảnh và PDF trực tiếp phối cảnh + thống kê ván.'
+    ext.version = '1.9.38'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
