1579675aa7cd5ce420b59021945c2ed565687031
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-15 17:18:56 +0700

    Hotfix bootstrap 1.9.2 and rotate manifest URL

--- tran_tuan_noi_that_release/files/tran_tuan_noi_that/bootstrap.rb
+++ tran_tuan_noi_that_release/files/tran_tuan_noi_that/bootstrap.rb
@@ -8,10 +8,14 @@ require 'tmpdir'
 require 'digest'
 
 module TranTuanNoiThat
-  ROOT = __dir__.freeze unless const_defined?(:ROOT)
-  VERSION = '1.9.1'.freeze unless const_defined?(:VERSION)
-  NAME = 'TRẦN TUẤN NỘI THẤT'.freeze unless const_defined?(:NAME)
-  MANIFEST_URL = 'https://raw.githubusercontent.com/tuanboidoi29-ai/TT-t-o-v-n-3d/main/tran_tuan_noi_that_release/update.json'.freeze unless const_defined?(:MANIFEST_URL)
+  ROOT = __dir__.freeze unless const_defined?(:ROOT, false)
+  NAME = 'TRẦN TUẤN NỘI THẤT'.freeze unless const_defined?(:NAME, false)
+
+  remove_const(:VERSION) if const_defined?(:VERSION, false)
+  VERSION = '1.9.2'.freeze
+
+  remove_const(:MANIFEST_URL) if const_defined?(:MANIFEST_URL, false)
+  MANIFEST_URL = 'https://raw.githubusercontent.com/tuanboidoi29-ai/TT-t-o-v-n-3d/main/tran_tuan_noi_that_release/update_latest.json'.freeze
 
   class << self
     def setting(key, default = nil)
@@ -88,7 +92,6 @@ module TranTuanNoiThat
     def install_box_ui
       return if @box_ui_installed
       return unless defined?(TranTuanNoiThat::Box)
-
       @box_ui_installed = true
       cmd = command('Tạo Khối BOX', 'box.svg', 'Tạo BOX khối đặc hoặc khung', :box) { Box.show_dialog }
       (@main_menu || UI.menu('Extensions')).add_item(cmd)
@@ -99,7 +102,6 @@ module TranTuanNoiThat
     def install_drawer_ui
       return if @drawer_ui_installed
       return unless defined?(TranTuanNoiThat::Drawer)
-
       @drawer_ui_installed = true
       cmd = command('Vẽ Ngăn Kéo', 'drawer.svg', 'Vẽ ngăn kéo 5 tấm theo vùng P1/P2', :drawer) { Drawer.activate }
       (@main_menu || UI.menu('Extensions')).add_item(cmd)
@@ -110,7 +112,6 @@ module TranTuanNoiThat
     def install_round_ui
       return if @round_ui_installed
       return unless defined?(TranTuanNoiThat::Round)
-
       @round_ui_installed = true
       cmd = command('Bo Cong Khối', 'bo_cong.svg', 'Bo cung lồi/lõm tại góc; TAB đổi chế độ', :round) { Round.activate }
       (@main_menu || UI.menu('Extensions')).add_item(cmd)
@@ -131,9 +132,7 @@ module TranTuanNoiThat
       cmd.large_icon = icon
       cmd.tooltip = title
       cmd.status_bar_text = description
-      if feature
-        cmd.set_validation_proc { feature_enabled?(feature) ? MF_ENABLED : MF_GRAYED }
-      end
+      cmd.set_validation_proc { feature_enabled?(feature) ? MF_ENABLED : MF_GRAYED } if feature
       cmd
     end
 
