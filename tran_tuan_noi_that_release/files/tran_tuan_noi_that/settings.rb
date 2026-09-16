cb5e4d4e7b0ef55b7189e857a6eb9c955a839d01
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 09:25:34 +0700

    Load Owner Admin V1.2.1 hotfix and bump system 1.9.49

--- tran_tuan_noi_that_release/files/tran_tuan_noi_that/settings.rb
+++ tran_tuan_noi_that_release/files/tran_tuan_noi_that/settings.rb
@@ -8,11 +8,13 @@ module TranTuanNoiThat
   load grain_patch_v350 if File.file?(grain_patch_v350)
   load grain_patch_v350_hotfix if File.file?(grain_patch_v350_hotfix)
 
-  # License: cache AppData -> Payment V1.1.0 -> Owner Admin V1.2.0.
+  # License: cache AppData -> Payment V1.1.0 -> Owner Admin V1.2.0 -> UI hotfix V1.2.1.
   license_payment_v110 = File.join(ROOT, 'license_payment_v110.rb')
   license_owner_admin_v120 = File.join(ROOT, 'license_owner_admin_v120.rb')
+  license_owner_admin_v121_fix = File.join(ROOT, 'license_owner_admin_v121_fix.rb')
   load license_payment_v110 if File.file?(license_payment_v110)
   load license_owner_admin_v120 if File.file?(license_owner_admin_v120)
+  load license_owner_admin_v121_fix if File.file?(license_owner_admin_v121_fix)
 
   # Layout load order bắt buộc:
   # base -> V0.3.0 -> compat -> V0.4.0 -> compat -> V0.5.0 -> V0.6.0 -> V0.7.0 -> V0.8.0 -> V0.8.1.
@@ -33,7 +35,7 @@ module TranTuanNoiThat
   load layout_patch_v081 if File.file?(layout_patch_v081)
 
   remove_const(:VERSION) if const_defined?(:VERSION, false)
-  VERSION = '1.9.48'.freeze
+  VERSION = '1.9.49'.freeze
 
   module Settings
     extend self
