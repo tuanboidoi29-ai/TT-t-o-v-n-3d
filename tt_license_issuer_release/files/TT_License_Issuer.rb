0193aa86724f879ed072eb5d4eb6194751fe7c1b
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-17 02:15:16 +0700

    Rename owner issuer extension to TRANTUAN-NOIITHAT-ADM v1.1.0

--- tt_license_issuer_release/files/TT_License_Issuer.rb
+++ tt_license_issuer_release/files/TT_License_Issuer.rb
@@ -4,9 +4,9 @@ require 'extensions.rb'
 
 module TTLicenseIssuerLoader
   unless file_loaded?(__FILE__)
-    ext = SketchupExtension.new('TRẦN TUẤN - CẤP MÃ BẢN QUYỀN OWNER', 'tt_license_issuer/main')
-    ext.description = 'Công cụ OWNER: nhập Mã máy khách, chọn thời hạn và tạo mã kích hoạt RSA Offline V2 để sao chép trực tiếp.'
-    ext.version = '1.0.0'
+    ext = SketchupExtension.new('TRANTUAN-NOIITHAT-ADM', 'tt_license_issuer/main')
+    ext.description = 'Công cụ OWNER khóa theo máy: nhập Mã máy khách, chọn 90/180 ngày hoặc Vĩnh viễn và cấp 1 mã RSA mở toàn bộ hệ thống.'
+    ext.version = '1.1.0'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
