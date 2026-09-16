916c7888f3abf253292a0a3a99c529221aee98e6
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 08:20:26 +0700

    Bump system to 1.9.45 License V1.0.1

--- tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
@@ -5,8 +5,8 @@ require 'extensions.rb'
 module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
-    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: License V1 theo Mã máy + quyền từng chức năng; Bo Cong V2.2.7 đã khóa; Xoay Vân V3.5.0; LayoutStats V0.8.1.'
-    ext.version = '1.9.44'
+    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: License V1.0.1 sửa hiển thị OWNER + quyền chức năng; Bo Cong V2.2.7 đã khóa; Xoay Vân V3.5.0; LayoutStats V0.8.1.'
+    ext.version = '1.9.45'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
