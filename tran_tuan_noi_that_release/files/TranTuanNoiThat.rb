b08cb69dfb3f111d54d07e61ac182823255fd11b
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 07:23:08 +0700

    Bump system 1.9.41 for LayoutStats V0.8.0

--- tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
@@ -5,8 +5,8 @@ require 'extensions.rb'
 module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
-    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Xoay Vân Ván V3.5.0 preview trước khi áp dụng, rule theo chi tiết, nhớ khổ theo vật liệu, khóa sản xuất và UV đúng tỷ lệ; Layout/PDF giữ V0.7.0.'
-    ext.version = '1.9.40'
+    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Xoay Vân Ván V3.5.0; LayoutStats V0.8.0 xuất 5 trang kỹ thuật gồm Tổng thể, Line+X-Ray, Khung LINE, Mặt đứng và Mặt cắt; PDF nối thêm thống kê ván.'
+    ext.version = '1.9.41'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
