26a465ce627cdb62e8e017b11b7a40074d260dd3
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 07:49:42 +0700

    Bump extension to 1.9.43 Bo Cong V2.2.7

--- tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
@@ -5,8 +5,8 @@ require 'extensions.rb'
 module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
-    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Xoay Vân Ván V3.5.0; LayoutStats V0.8.1 sửa khoảng cắt thật từ mặt ngoài cho 3 mặt cắt, giữ 5 trang kỹ thuật và PDF nối thống kê.'
-    ext.version = '1.9.42'
+    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: Bo Cong Khối V2.2.7 giữ chắc 2 đường biên đầu/cuối theo topology; Xoay Vân V3.5.0; LayoutStats V0.8.1.'
+    ext.version = '1.9.43'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
