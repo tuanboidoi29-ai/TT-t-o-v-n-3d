624c8b15629b7c1285a7209da02dab2d39b6ac25
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 12:47:46 +0700

    Bump extension to 1.9.53 automatic license payment

--- tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
@@ -5,8 +5,8 @@ require 'extensions.rb'
 module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
-    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: License Commercial V1.3.0 thêm cấu hình ngân hàng/QR trong OWNER và QR thanh toán cho khách; Bo Cong V2.2.7 đã khóa; Xoay Vân V3.5.0; LayoutStats V0.8.1.'
-    ext.version = '1.9.52'
+    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: License Commercial V1.4.0 tự nhận thanh toán SePay, tự kiểm tra giao dịch và mở quyền; Bo Cong V2.2.7 đã khóa; Xoay Vân V3.5.0; LayoutStats V0.8.1.'
+    ext.version = '1.9.53'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
