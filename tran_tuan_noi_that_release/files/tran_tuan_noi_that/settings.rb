fedd86c1367d0762b33c7a6810eb3def81a66c92
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 13:25:38 +0700

    Bump system source to 1.9.54 updater bridge transition

--- tran_tuan_noi_that_release/files/tran_tuan_noi_that/settings.rb
+++ tran_tuan_noi_that_release/files/tran_tuan_noi_that/settings.rb
@@ -43,7 +43,7 @@ module TranTuanNoiThat
   load layout_patch_v081 if File.file?(layout_patch_v081)
 
   remove_const(:VERSION) if const_defined?(:VERSION, false)
-  VERSION = '1.9.53'.freeze
+  VERSION = '1.9.54'.freeze
 
   module Settings
     extend self
