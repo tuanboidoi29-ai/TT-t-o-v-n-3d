012e28374284150a4f3ec80face96cd556921f28
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-16 09:31:36 +0700

    Bump extension to 1.9.50 License UI V1.2.2

--- tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
+++ tran_tuan_noi_that_release/files/TranTuanNoiThat.rb
@@ -5,8 +5,8 @@ require 'extensions.rb'
 module TranTuanNoiThat
   unless file_loaded?(__FILE__)
     ext = SketchupExtension.new('TRẦN TUẤN NỘI THẤT', 'tran_tuan_noi_that/bootstrap')
-    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: License Owner Admin V1.2.1 sửa lỗi bảng Bản Quyền mở ra nhưng Mã máy/trạng thái/quyền không render; Bo Cong V2.2.7 đã khóa; Xoay Vân V3.5.0; LayoutStats V0.8.1.'
-    ext.version = '1.9.49'
+    ext.description = 'Hệ thống công cụ nội thất TRẦN TUẤN: License UI V1.2.2 sửa lỗi JavaScript khiến Bản Quyền chỉ hiện dấu - và 0/0; Owner Admin giữ nguyên; Bo Cong V2.2.7 đã khóa; Xoay Vân V3.5.0; LayoutStats V0.8.1.'
+    ext.version = '1.9.50'
     ext.creator = 'TRẦN TUẤN'
     ext.copyright = '2026 TRẦN TUẤN'
     Sketchup.register_extension(ext, true)
