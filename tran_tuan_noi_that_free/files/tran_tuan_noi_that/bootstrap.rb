1ba4e1e78ca64ea4c5fc28bf04ce8cd073115ad6
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-21 22:30:55 +0700

    feat: integrate CAM CHOT tool into runtime 1.9.107

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/bootstrap.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/bootstrap.rb
@@ -11,7 +11,7 @@ module TranTuanNoiThat
   NAME = 'TRẦN TUẤN NỘI THẤT'.freeze unless const_defined?(:NAME, false)
 
   remove_const(:VERSION) if const_defined?(:VERSION, false)
-  VERSION = '1.9.106'.freeze
+  VERSION = '1.9.107'.freeze
 
   class << self
     def setting(key, default = nil)
@@ -88,6 +88,7 @@ module TranTuanNoiThat
         rename_ui
         rename_tool
         tam_pro
+        cam_chot_tool
         box_tool
         drawer_tool
         round_tool
@@ -140,6 +141,7 @@ module TranTuanNoiThat
       install_cabinet_door_ui
       install_rename_ui
       install_tam_pro_ui
+      install_cam_chot_ui
       install_round_ui
       install_stretch_mode_ui
       install_grain_ui
@@ -186,6 +188,20 @@ module TranTuanNoiThat
       true
     rescue StandardError => error
       puts "[TT UI TamPro] #{error.class}: #{error.message}"
+      false
+    end
+
+    def install_cam_chot_ui
+      return false unless defined?(TranTuanNoiThat::CamChot)
+      @cam_chot_cmd ||= command(
+        'Liên Kết CAM - CHỐT',
+        'cam_chot.svg',
+        'Bắt 2 tấm vuông góc · CAM Ø15 / B34 · TAB đảo mặt CAM · ENTER tạo'
+      ) { CamChot.show }
+      add_feature_command_once(@cam_chot_cmd, :cam_chot_menu_installed)
+      true
+    rescue StandardError => error
+      puts "[TT UI CamChot] #{error.class}: #{error.message}"
       false
     end
 
