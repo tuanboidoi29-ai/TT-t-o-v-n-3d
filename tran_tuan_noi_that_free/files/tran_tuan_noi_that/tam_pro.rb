c7594ceb8d0275b8469917949ab95c5abf6947b0
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-21 23:49:00 +0700

    release: bump CAM outline-only fix to 1.9.111

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/tam_pro.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/tam_pro.rb
@@ -6,7 +6,7 @@ module TranTuanNoiThat
   module TamPro
     extend self
 
-    VERSION = '1.9.110'.freeze
+    VERSION = '1.9.111'.freeze
     EPS = 0.001
     QUICK_NAMES = ['Trái', 'Phải', 'Trên', 'Dưới', 'Trước', 'Sau'].freeze
 
--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/updater.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/updater.rb
@@ -241,7 +241,7 @@ module TranTuanNoiThat
       raise 'Máy chủ cập nhật không hợp lệ.' unless %w[raw.githubusercontent.com api.github.com].include?(uri.host)
 
       headers = {
-        'User-Agent' => 'TranTuanNoiThat-SketchUp/1.9.110',
+        'User-Agent' => 'TranTuanNoiThat-SketchUp/1.9.111',
         'Cache-Control' => 'no-cache, no-store, max-age=0',
         'Pragma' => 'no-cache'
       }
