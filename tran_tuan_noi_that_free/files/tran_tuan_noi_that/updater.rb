f8e9fcc755ccd81e03382dfc637961b1ed15c323
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-21 21:52:50 +0700

    release: publish 1.9.105 with Ruby syntax guard

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/updater.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/updater.rb
@@ -241,7 +241,7 @@ module TranTuanNoiThat
       raise 'Máy chủ cập nhật không hợp lệ.' unless %w[raw.githubusercontent.com api.github.com].include?(uri.host)
 
       headers = {
-        'User-Agent' => 'TranTuanNoiThat-SketchUp/1.9.104',
+        'User-Agent' => 'TranTuanNoiThat-SketchUp/1.9.105',
         'Cache-Control' => 'no-cache, no-store, max-age=0',
         'Pragma' => 'no-cache'
       }
