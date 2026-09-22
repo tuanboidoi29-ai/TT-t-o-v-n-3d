f3e16ad8d967d4cb49de796d041183b7275e40b2
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-22 11:30:18 +0700

    release: bump Scale 4 Cạnh to 1.9.127

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/scale_corner_lock.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/scale_corner_lock.rb
@@ -22,7 +22,7 @@ module TranTuanNoiThat
   module ScaleCornerLock
     extend self
 
-    VERSION = '1.9.126'.freeze
+    VERSION = '1.9.127'.freeze
     PICK_RADIUS = 20.0
     MIN_FACTOR = 0.001
 
