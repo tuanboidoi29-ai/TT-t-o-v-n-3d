8d84b04d2488f4b274c01a3c76821be9dc724a9d
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-22 09:37:08 +0700

    release: bump Tam PRO to 1.9.118

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/tam_pro.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/tam_pro.rb
@@ -6,7 +6,7 @@ module TranTuanNoiThat
   module TamPro
     extend self
 
-    VERSION = '1.9.117'.freeze
+    VERSION = '1.9.118'.freeze
     EPS = 0.001
     QUICK_NAMES = ['Trái', 'Phải', 'Trên', 'Dưới', 'Trước', 'Sau'].freeze
 
