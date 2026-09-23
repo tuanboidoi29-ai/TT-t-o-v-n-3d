4913958b62fd49052e7d27c41186abedd8ddf975
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-23 11:39:42 +0700

    release: bump runtime to 1.9.133

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/bootstrap.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/bootstrap.rb
@@ -11,7 +11,7 @@ module TranTuanNoiThat
   NAME = 'TRẦN TUẤN NỘI THẤT'.freeze unless const_defined?(:NAME, false)
 
   remove_const(:VERSION) if const_defined?(:VERSION, false)
-  VERSION = '1.9.132'.freeze
+  VERSION = '1.9.133'.freeze
 
   class << self
     def setting(key, default = nil)
