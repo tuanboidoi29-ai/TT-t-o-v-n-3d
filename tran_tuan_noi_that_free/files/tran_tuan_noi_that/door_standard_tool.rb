2ada206b9cbe5a6bef1b8b64078b7ce6d39f3251
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-23 11:13:04 +0700

    fix: remove extra module end after door feature merge

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/door_standard_tool.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/door_standard_tool.rb
@@ -1173,7 +1173,6 @@ module TranTuanNoiThat
       end
     end
   end
-  end
 
   # Tương thích nóng cho UI::Command cũ trong phiên SketchUp đang mở.
   # Source Vẽ Cánh Tủ cũ đã bị gỡ; lệnh cũ nếu còn trên toolbar sẽ gọi tool mới.
