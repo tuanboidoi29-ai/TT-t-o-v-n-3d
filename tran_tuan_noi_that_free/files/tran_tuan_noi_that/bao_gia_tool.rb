faf682bcb578dbfe87cbea247188ce01c251745c
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-23 11:31:41 +0700

    release: bump Bao Gia to 1.9.132

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/bao_gia_tool.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/bao_gia_tool.rb
@@ -28,7 +28,7 @@ module TranTuanNoiThat
   module BaoGiaTool
     extend self
 
-    VERSION = '1.9.131'.freeze
+    VERSION = '1.9.132'.freeze
     ROOT = File.join(TranTuanNoiThat::ROOT, 'data', 'bao_gia').freeze
 
     DEFAULT_DVT = [
