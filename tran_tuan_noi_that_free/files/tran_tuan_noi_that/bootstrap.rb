dc710bcea6c08f40c28a6925a09b8113ccbc4df7
Tran Dinh Tuấn <tuanboidoi29@gmail.com>
2026-09-23 11:50:59 +0700

    release: bump runtime to 1.9.134

--- tran_tuan_noi_that_free/files/tran_tuan_noi_that/bootstrap.rb
+++ tran_tuan_noi_that_free/files/tran_tuan_noi_that/bootstrap.rb
@@ -11,7 +11,7 @@ module TranTuanNoiThat
   NAME = 'TRẦN TUẤN NỘI THẤT'.freeze unless const_defined?(:NAME, false)
 
   remove_const(:VERSION) if const_defined?(:VERSION, false)
-  VERSION = '1.9.133'.freeze
+  VERSION = '1.9.134'.freeze
 
   class << self
     def setting(key, default = nil)
@@ -162,7 +162,7 @@ module TranTuanNoiThat
         if instance_variable_defined?(:@cabinet_door_cmd) && @cabinet_door_cmd
           title = 'Tạo Cánh Chuẩn'
           @cabinet_door_cmd.tooltip = title
-          @cabinet_door_cmd.status_bar_text = 'P1-P2 chéo · preview 3D · / chia cánh · SHIFT Dọc/Ngang · CTRL Lọt/Phủ · TAB cài đặt.'
+          @cabinet_door_cmd.status_bar_text = 'P1-P2 chéo tự do · TÂM động theo chuột · / hoặc 1 chia tự do · SHIFT Dọc/Ngang · CTRL Lọt/Phủ · TAB cài đặt.'
           @cabinet_door_cmd.menu_text = title if @cabinet_door_cmd.respond_to?(:menu_text=)
           icon = File.join(ROOT, 'icons', 'door_standard.svg')
           if File.file?(icon)
